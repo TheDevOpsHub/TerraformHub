5f2e29a52e9e830e6f6498d8fd2810c24563115d
Tung Bui <tung.bquang@gmail.com>
2024-07-25 12:42:12 +0700

    aws: fix gitlab runner token missing in userdata script

--- AWS/aws-ec2-gitlab-runner/main.tf
+++ AWS/aws-ec2-gitlab-runner/main.tf
@@ -19,12 +19,14 @@ resource "aws_security_group" "gitlab_runner" {
 
 resource "aws_instance" "gitlab_runner" {
   # Check all available us-east-1 AMIs at: https://us-east-1.console.aws.amazon.com/ec2/home?region=us-east-1#AMICatalog
-  ami                    = "ami-03972092c42e8c0ca" # Amazon Linux 2 AMI
-  instance_type          = var.instance_type
-  key_name               = var.key_name
-  security_groups        = [aws_security_group.gitlab_runner.name]
+  ami             = "ami-03972092c42e8c0ca" # Amazon Linux 2 AMI
+  instance_type   = var.instance_type
+  key_name        = var.key_name
+  security_groups = [aws_security_group.gitlab_runner.name]
 
-  user_data              = file("install_runner.sh")
+  user_data = templatefile("install_runner.tpl", {
+    gitlab_runner_registration_token = var.gitlab_runner_registration_token
+  })
 
   tags = {
     Name = "AWS EC2 GitLab Runner"
